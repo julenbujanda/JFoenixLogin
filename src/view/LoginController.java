326647acf2326d1ce78d7a776fc1c1bb94983c9d
julenb <21701719@live.uem.es>
2018-10-31 18:28:37 +0100

    Transición

--- src/view/LoginController.java
+++ src/view/LoginController.java
@@ -1,15 +1,101 @@
 package view;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class LoginController {
+
+    @FXML
+    private Pane pane1;
+
+    @FXML
+    private Pane pane2;
+
+    @FXML
+    private Pane pane3;
+
+    @FXML
+    private Pane pane4;
+
+    public void initialize() {
+        pane1.setStyle("-fx-background-image: url(img/fade1.jpg); -fx-background-size: cover");
+        pane2.setStyle("-fx-background-image: url(img/fade2.jpg);  -fx-background-size: cover");
+        pane3.setStyle("-fx-background-image: url(img/fade3.jpg); -fx-background-size: cover");
+        pane4.setStyle("-fx-background-image: url(img/fade4.jpg); -fx-background-size: cover");
+        animacionFondo();
+    }
+
+    private void animacionFondo() {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5),
+                pane4);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+        fadeTransition
+                .setOnFinished(event -> {
+
+                    FadeTransition fadeTransition1 = new FadeTransition(
+                            Duration.seconds(3), pane3);
+                    fadeTransition1.setFromValue(1);
+                    fadeTransition1.setToValue(0);
+                    fadeTransition1.play();
+
+                    fadeTransition1.setOnFinished(event1 -> {
+                        FadeTransition fadeTransition2 = new FadeTransition(
+                                Duration.seconds(3), pane2);
+                        fadeTransition2.setFromValue(1);
+                        fadeTransition2.setToValue(0);
+                        fadeTransition2.play();
+
+                        fadeTransition2.setOnFinished(event2 -> {
+
+                            FadeTransition fadeTransition0 = new FadeTransition(
+                                    Duration.seconds(3), pane2);
+                            fadeTransition0.setFromValue(0);
+                            fadeTransition0.setToValue(1);
+                            fadeTransition0.play();
+
+                            fadeTransition0.setOnFinished(event3 -> {
+
+                                FadeTransition fadeTransition11 = new FadeTransition(
+                                        Duration.seconds(3), pane3);
+
+                                fadeTransition11.setFromValue(0);
+                                fadeTransition11.setToValue(1);
+                                fadeTransition11.play();
+
+                                fadeTransition11.setOnFinished(event4 -> {
+
+                                    FadeTransition fadeTransition22 = new FadeTransition(
+                                            Duration.seconds(3), pane4);
+
+                                    fadeTransition22.setFromValue(0);
+                                    fadeTransition22.setToValue(1);
+                                    fadeTransition22.play();
+
+                                    fadeTransition22.setOnFinished(event5 -> {
+
+                                        animacionFondo();
+                                    });
+
+                                });
+
+                            });
+
+                        });
+                    });
+
+                });
+    }
 
     @FXML
     private void openSignUp() {
