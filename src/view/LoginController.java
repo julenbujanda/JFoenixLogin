00ea71e2b4377da3bd25adde89766192bb63fdc3
julenb <21701719@live.uem.es>
2018-10-31 18:01:44 +0100

    Controlador

--- src/view/LoginController.java
+++ src/view/LoginController.java
@@ -1,4 +1,30 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private void openSignUp() {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.getIcons().add(new Image("file:src/img/plane.png"));
+        stage.setTitle("Sign Up");
+        stage.show();
+    }
+
 }
