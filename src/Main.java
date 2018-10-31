3a8e2f3fc7347fec4070b7bb9b6be998fb845329
julenb <21701719@live.uem.es>
2018-10-31 20:33:24 +0100

    Esquinas redondeadas, mover ventana

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -10,14 +14,35 @@ public class Main extends Application {
 
     private AnchorPane myPane;
 
+    private double xOffset;
+    private double yOffset;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         myPane = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+        Scene scene=new Scene(myPane);
         primaryStage.getIcons().add(new Image("file:src/img/plane.png"));
         primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.UNDECORATED);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Rectangle rect = new Rectangle(520,525);
+        rect.setArcHeight(60.0);
+        rect.setArcWidth(60.0);
+        myPane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = primaryStage.getX() - event.getScreenX();
+                yOffset = primaryStage.getY() - event.getScreenY();
+            }
+        });
+        myPane.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
+        myPane.setClip(rect);
         primaryStage.setTitle("Trip Log In");
-        primaryStage.setScene(new Scene(myPane));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
